e60494823d07e8731568239415ccf7150f06e122
Tim Li <urumican@163.com>
2020-08-16 22:43:25 +0800

    add add post

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -2,13 +2,18 @@ import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = [
   { id: '1', title: 'First Post!', content: 'Hello!' },
-  { id: '2', title: 'Second Post', content: 'More text' }
+  { id: '2', title: 'Second Post', content: 'More text' },
 ]
 
 const postsSlice = createSlice({
   name: 'posts',
   initialState,
-  reducers: {}
+  reducers: {
+    postAdded(state, action) {
+      state.push(action.payload)
+    },
+  },
 })
 
+export const { postAdded } = postsSlice.actions
 export default postsSlice.reducer
